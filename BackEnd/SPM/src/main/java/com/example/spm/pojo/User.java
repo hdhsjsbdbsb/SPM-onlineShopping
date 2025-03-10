9a1b5453593a1247a2614ef2738bd1984b97e7ad
zhouminghao1 <1656047240@qq.com>
2025-03-10 11:21:45 +0800

    补正,并添加了用户的信息修改功能

--- BackEnd/SPM/src/main/java/com/example/spm/pojo/User.java
+++ BackEnd/SPM/src/main/java/com/example/spm/pojo/User.java
@@ -10,18 +10,9 @@ import java.time.LocalTime;
 @Data
 public class User {
     private int id;
-    @NotBlank(message = "用户名不能为空")
-    @Pattern(regexp = "^\\S{1,10}$", message = "用户名格式错误")
     private String username;
-
-    @NotBlank(message = "密码不能为空")
-    @Pattern(regexp = "^\\S{6,16}$", message = "密码格式错误")
     private String password;
-
-    @Pattern(regexp = "^(?:\\+?86)?1[3-9]\\d{9}$", message = "手机号格式错误")
     private String phone;
-    @Email
-    @NotEmpty
     private String email;
     private String nickname; //签名
     private String userPic;//用户头像地址;
